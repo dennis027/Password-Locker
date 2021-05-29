8fadf0d3e9c39ff7cf92a0e90951eaf5a16bcde3
Dennis <dennis.kimani@student.moringaschool.com>
2021-05-29 23:24:31 +0300

    creating the scope of classes

--- password.py
+++ password.py
@@ -1,7 +1,13 @@
 class User:
-    pass
+    "generate a class for the users"
+    def __init__(self, create_account,login):
+        self.name = create_account
+        self.login = login
 
 
 
 class Credentials:
-    pass
+    "generates a class for the credentials"
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
