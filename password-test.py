a3a4e66439a5e6580cfad8a4ba05b49d05560395
Dennis <dennis.kimani@student.moringaschool.com>
2021-05-30 22:38:20 +0300

    adding testclass for deleting contacts

--- password-test.py
+++ password-test.py
@@ -14,6 +14,13 @@ class TestUser(unittest.TestCase):
         """
         self.new_user = User(True , False)# creates a contact object
         self.new_credentials = Credentials("dennis","bandict123")
+
+    def tearDown(self):
+        """
+        tearDown method that does clean up after each test case has run
+        """
+        Credentials.credentials_list = []
+
     def test_init(self):
         """
         test_init method checking if object is initialised properly
@@ -40,9 +47,27 @@ class TestUser(unittest.TestCase):
         self.assertEqual(len(Credentials.credentials_list),1)
 
 
-    def test_save_multiple_credentials(self):
-        
+ 
 
+
+    def test_save_credentials(self):
+        """
+        test_save_multiple_credentials
+        to check if we can save multiple credentials
+        objects to our credential list
+        """  
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials ("kimani","mee")   #new credentials .save
+        self.assertEqual (len(Credentials.credentials_list),1)
+
+    def test_delete_credentials(self):
+        """
+        test if we can remove a credential from our application
+        """
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials ("kimani","mee")#new account user_list
+        self.new_credentials.delete_credentials()#delete credential
+        self.assertEqual(len(Credentials.credentials_list),0)
 
 
 
