6e184bfce5e900a90127773769c6b4b01be6c12b
Dennis <dennis.kimani@student.moringaschool.com>
2021-05-30 13:39:15 +0300

    adding a unitest class for credential  class

--- password-test.py
+++ password-test.py
@@ -13,11 +13,16 @@ class TestUser(unittest.TestCase):
         setup method helping us run each testcase
         """
         self.new_user = User(True , False)# creates a contact object
+        self.new_credentials = Credentials("dennis","bandict123")
     def test_init(self):
         """
         test_init method checking if object is initialised properly
         """
         self.assertEqual(self.new_user.create_account,True)
         self.assertEqual(self.new_user.login,False)
+        self.assertEqual(self.new_credentials.username, "dennis")
+        self.assertEqual(self.new_credentials.password,"bandict123") 
 if __name__ == '__main__':
-    unittest.main()        
+    unittest.main()  
+
+
